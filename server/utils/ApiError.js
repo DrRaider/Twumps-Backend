f05ddbadfaaa84c68666ecc61ec0e7d406bc660a
Nicolas SAILLY <nsailly@live.fr>
2018-10-03 17:01:04 +0200

    Linted the seed with Standardjs

--- server/utils/ApiError.js
+++ server/utils/ApiError.js
@@ -1,13 +1,13 @@
-const ApplicationError = require('./ApplicationError');
+const ApplicationError = require('./ApplicationError')
 
 class ApiError extends ApplicationError {
-    constructor (api_name, message) {
-        // Providing default message and overriding status code.
-        //super('Could not complete request : ' + api_name + ', err: ' + message);
-        super('Impossible de completer la requete : ' + api_name + ', err: ' + message);
-    }
+  constructor (apiName, message) {
+    // Providing default message and overriding status code.
+    // super('Could not complete request : ' + api_name + ', err: ' + message);
+    super('Impossible de completer la requete : ' + apiName + ', err: ' + message)
+  }
 }
 
 module.exports = {
-    ApiError
-};
+  ApiError
+}
