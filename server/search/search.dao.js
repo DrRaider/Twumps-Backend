4883d799aaa15c8569f6788fc4002d401d10685c
sodsixela <alexisdos@hotmail.fr>
2018-10-20 13:57:53 +0200

    Upgraded research

--- server/search/search.dao.js
+++ server/search/search.dao.js
@@ -1,9 +1,12 @@
 const sqlite = require('../sqlite')
 
 const getSearch = async (keyword) => {
-  return sqlite.all('SELECT content FROM tweets WHERE content like "%' + keyword + '%" ORDER BY CONTENT DESC LIMIT 20')
+  return  sqlite.all('SELECT content FROM tweets WHERE content like "%' + keyword + '%" ORDER BY Created_at DESC LIMIT 20')
 }
 
+const getCount = async (keyword) => {
+  return sqlite.all('SELECT COUNT(content) as count FROM tweets WHERE content like "%' + keyword + '%"')
+}
 module.exports = {
-  getSearch
+  getSearch,getCount
 }
--- server/search/search.js
+++ server/search/search.js
@@ -3,8 +3,12 @@ const searchDao = require('./search.dao')
 const getSearch = async (keyword) => {
   return searchDao.getSearch(keyword)
     .then((data) => {
-      return data
-    })
+      let content= {data:{}, count : 0}
+      content.data= data
+      return searchDao.getCount(keyword)
+    .then((data) => { content.count = data[0].count
+      return content
+    })})
     .catch((err) => {
       throw err
     })
