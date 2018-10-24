3557285c526ce9eb6a0612c6daf6c8beb342c3f6
sodsixela <alexisdos@hotmail.fr>
2018-10-24 21:23:23 +0200

    Tweet like search

--- server/search/search.dao.js
+++ server/search/search.dao.js
@@ -1,7 +1,7 @@
 const sqlite = require('../sqlite')
 
 const getSearch = async (keyword) => {
-  return sqlite.all('SELECT CONTENT FROM TWEETS WHERE CONTENT LIKE "%' + keyword + '%" ORDER BY CREATED_AT DESC LIMIT 20')
+  return sqlite.all('SELECT CONTENT,retweet_count,favorite_count,created_at FROM TWEETS WHERE CONTENT LIKE "%' + keyword + '%" ORDER BY retweet_count DESC LIMIT 20')
 }
 
 const getCount = async (keyword) => {
