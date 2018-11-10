15c9f2bac69e8d96bd58e182c3a5e5aed5eee085
Frédéric ROSSO <frederic.rosso@efrei.net>
2018-11-10 22:18:08 +0100

    JSON file usefull for the world map

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(express.static('data/img'))
+app.use(express.static('data/json'))
 
 // Call app routers
 require('./server/app')(app)
