0db22078f2bf10e462e2cf7b9953a9f514129f93
sodsixela <alexisdos@hotmail.fr>
2018-11-11 19:08:11 +0100

    higher nb of tweet in timeline

--- server/timeline/timeline.dao.js
+++ server/timeline/timeline.dao.js
@@ -1,7 +1,7 @@
 const sqlite = require('../sqlite')
 
 async function getRetweet () {
-  return sqlite.all('SELECT id_str,CREATED FROM tweets ORDER BY retweet_count DESC LIMIT 10', [])
+  return sqlite.all('SELECT id_str,CREATED FROM tweets ORDER BY retweet_count DESC LIMIT 15', [])
 }
 
 module.exports = {
