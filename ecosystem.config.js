957a5902520188a315551622d2435344d102f1a4
Nicolas SAILLY <nsailly@live.fr>
2018-11-11 22:52:10 +0100

    testing prod deploymment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,10 @@ module.exports = {
     production: {
       user: 'ec2-user',
       host: 'ec2-35-180-192-229.eu-west-3.compute.amazonaws.com',
-      key: '~/.ssh/twumps.pem',
+      key: '/home/raider/.ssh/twumps.pem',
       ref: 'origin/getapi',
       repo: 'git@github.com:DrRaider/Twumps-BackEnd.git',
-      path: '/',
+      path: '/home/ec2-user/twumps',
       'post-deploy': 'yarn install && pm2 startOrRestart ecosystem.config.js'
     }
   }
